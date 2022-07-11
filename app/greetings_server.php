bd2827cbde77c77b799665c6ca2121c96992c50d
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-07-11 12:42:21 +0200

    Adding WSDL creator

--- app/greetings_server.php
+++ app/greetings_server.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
 class GreetingsServer
 {
     function sayHello(string $name): string
@@ -13,6 +15,15 @@ class GreetingsServer
     }
 }
 
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$currentURL = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+if (array_key_exists('wsdl', $_GET)) {
+    $wsdl = new WSDL\WSDLCreator(GreetingsServer::class, $currentURL);
+    $wsdl->setNamespace($protocol.$_SERVER['HTTP_HOST']);
+
+    die($wsdl->renderWSDL());
+}
 $server = new SoapServer(__DIR__.'/grettings.wsdl');
 
 $server->setClass(GreetingsServer::class);
