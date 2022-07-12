36f4cf86e3f4634f4247dde4d4d0b9af0831678c
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-07-12 15:16:07 +0200

    Fixing problems (Specially the binding style)

--- app/greetings_server.php
+++ app/greetings_server.php
@@ -1,32 +1,55 @@
 <?php
 
+const SERVER_URI = 'http://localhost:8000/greetings_server.php';
 require_once 'vendor/autoload.php';
+
+use Laminas\Soap\AutoDiscover;
 
 class GreetingsServer
 {
+    /**
+     * @param string $name
+     * @return string
+     */
     function sayHello(string $name): string
     {
         return "Hello $name!";
     }
 
-    function sayGoodBye(string $name): string
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function sayGoodBye(string $name): string
     {
         return "Goodbye $name!";
     }
 }
 
+$wsdl = (new AutoDiscover())
+    ->setClass(GreetingsServer::class)
+    ->setUri(SERVER_URI)
+    ->setServiceName('Greetings')
+    ->setOperationBodyStyle([
+        'use' => 'literal'
+    ])
+    ->setBindingStyle(
+        [
+            'style' => 'rpc'
+        ])
+    ->generate()
+    ->toXml();
+
+$wsdlfile = tempnam(sys_get_temp_dir(), __FILE__);
+file_put_contents($wsdlfile, $wsdl);
+
 if (array_key_exists('wsdl', $_GET)) {
-    die(
-    (new Laminas\Soap\AutoDiscover())
-        ->setClass(GreetingsServer::class)
-        ->setUri('https://leewayweb.com/soap/server')
-        ->setServiceName('Greetings')
-        ->generate()
-        ->toXml()
-    );
+    header('Content-Type: application/wsdl+xml');
+    die($wsdl);
 }
 
-$server = new SoapServer();
-
+$server = new SoapServer($wsdlfile, [
+    'soap_version' => SOAP_1_2,
+]);
 $server->setClass(GreetingsServer::class);
 $server->handle();
